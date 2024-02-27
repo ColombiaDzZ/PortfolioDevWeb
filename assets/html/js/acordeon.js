cd3abddcdb77bb555ad751e4024011f7f1cc8e3f
Vinicios <157557169+ColombiaDzZ@users.noreply.github.com>
2024-02-27 03:36:37 -0300

    finish

---
+++ assets/html/js/acordeon.js
@@ -0,0 +1,14 @@
+const acordeonTrigers = document.querySelectorAll('.acordeon .trigger')
+
+acordeonTrigers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const acordeon = trigger.parentElement
+        const isOpen = acordeon.classList.contains('open')
+
+        if (isOpen) {
+            acordeon.classList.remove('open') 
+        } else {
+            acordeon.classList.add('open') 
+        }
+    })
+})
